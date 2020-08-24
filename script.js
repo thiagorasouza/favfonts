f521db79100b80b945956f98634499a99d3bf50d
Thiago Souza <thiagorasouza@gmail.com>
2020-08-24 20:40:12 -0300

    add dynamic functionality

---
+++ script.js
@@ -0,0 +1,32 @@
+const customText = document.querySelector('#input-text');
+const cardsText = document.querySelectorAll('.card-text');
+const selectSize = document.querySelector('#select-size');
+const resetBtn = document.querySelector('#btn-reset');
+const defaultText = 'The quick brown fox jumps over the lazy dog.';
+
+function reset() {
+  cardsText.forEach(cardText => {
+    cardText.textContent = defaultText;
+    cardText.style = '';
+  });
+};
+
+reset();
+
+function changeText() {
+  let customText = this.value || defaultText;
+  cardsText.forEach(cardText => {
+    cardText.textContent = customText;
+  });
+}
+
+function changeSize() {
+  let fontSize = parseInt(this.value);
+  cardsText.forEach(cardText => {
+    cardText.style = `font-size: ${fontSize}px;`;
+  });
+}
+
+customText.addEventListener('input', changeText);
+selectSize.addEventListener('input', changeSize);
+resetBtn.addEventListener('click', reset);
